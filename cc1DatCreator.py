285a27077b3dd55312d278a25d249adc1fc1d2de
Drewroen <drewroen@gmail.com>
2020-11-18 22:33:26 -0600

    Make an ending level and remove the password thing

--- cc1DatCreator.py
+++ cc1DatCreator.py
@@ -19,25 +19,19 @@ def create_random_dat(levels_list, number_of_levels):
     # Add the initial data, number of levels
     random_dat = b''
     random_dat += b'\xac\xaa\x02\x00'
-    random_dat += (number_of_levels).to_bytes(1, "little")
-    random_dat += b'\x00'
+    random_dat += (number_of_levels + 2).to_bytes(2, "little")
 
     # Level 1 will always be just to load the map, free finish
     random_dat += b'\x8a\x00\x01\x00\x00\x00\x00\x00\x01\x00Y\x00\xff!\x00\xff\t9\xff\x17\x009\xff\x07;9\xff\x17\x009\xff\x07;9\xff\x17\x009;;\x15\x15\x15;;9\xff\x17\x009;;\x15n\x15;;9\xff\x17\x009;;\x15\x15\x15;;9\xff\x17\x009\xff\x07;9\xff\x17\x009\xff\x07;9\xff\x17\x00\xff\t9\xff\xff\x00\xff\xff\x00\xff\xd8\x00\x0f\x00\xff\xff\x00\xff\xff\x00\xff\xff\x00\xff\xff\x00\xff\x04\x00\x14\x00\x03\x0bGood Luck!\x00\x06\x05\xd8\xd8\xd8\xd8\x00'
 
     # For each level, add a random level that was scraped, but replace all the passwords with AAAA
-    for x in range(1, number_of_levels):
+    for x in range(number_of_levels):
         random_level = random.choice(levels_list)
-        modified_random_level = random_level[0:2] + (x+1).to_bytes(2, "little") + random_level[4:]
+        modified_random_level = random_level[0:2] + (x+2).to_bytes(2, "little") + random_level[4:]
+        random_dat += modified_random_level
 
-        first_layer_size = int.from_bytes(modified_random_level[10:12], "little")
-        second_layer_size = int.from_bytes(modified_random_level[12+first_layer_size:14+first_layer_size], "little")
-        non_map_section_index = 16+first_layer_size+second_layer_size
-        for y in range(non_map_section_index, len(modified_random_level) - 2):
-            if modified_random_level[y:y+2] == b'\x06\x05' and modified_random_level[y+6:y+7] == b'\x00':
-                modified_random_level = modified_random_level[0:y+2] + b'\xd8\xd8\xd8\xd8' + modified_random_level[y+6:]
-                random_dat += modified_random_level
-                break
+    # Last level can't be finished so you know it's the end
+    random_dat += b'\x8a\x00' + (number_of_levels + 2).to_bytes(2, "little") + b'\x00\x00\x00\x00\x01\x00Y\x00\xff!\x00\xff\t9\xff\x17\x009\xff\x07;9\xff\x17\x009\xff\x07;9\xff\x17\x009;;\x00\x00\x00;;9\xff\x17\x009;;\x00n\x00;;9\xff\x17\x009;;\x00\x00\x00;;9\xff\x17\x009\xff\x07;9\xff\x17\x009\xff\x07;9\xff\x17\x00\xff\t9\xff\xff\x00\xff\xff\x00\xff\xd8\x00\x0f\x00\xff\xff\x00\xff\xff\x00\xff\xff\x00\xff\xff\x00\xff\x04\x00\x14\x00\x03\x0bGo Back!!!\x00\x06\x05\xdb\xdb\xdb\xdb\x00'
 
     return random_dat
 
